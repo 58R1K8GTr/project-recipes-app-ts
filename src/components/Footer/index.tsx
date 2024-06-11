c7023ce13367aa4570f134e0fc0096245580f976
Mario Burali <marioburali@hotmail.com>
2024-06-11 14:59:43 -0300

    [Group 2] Requirements 15-17

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./style.css";
+import drinkIcon from '../../images/drinkIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
+import { useNavigate } from 'react-router-dom';
+
+function Footer() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <footer data-testid="footer" id='footer' className='footer'>
+
+        <button className="footer-btn" onClick={() => navigate('/meals')} data-testid="meals-btn">
+          <img src={mealIcon} alt="Ícone de Comida" data-testid="meals-bottom-btn" />
+        </button>
+
+        <button className="footer-btn" onClick={() => navigate('/drinks')} data-testid="drinks-btn">
+          <img src={drinkIcon} alt="Ícone de Bebida" data-testid="drinks-bottom-btn" />
+        </button>
+
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/tests/Footer.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Footer from '../components/Footer/index';
+import { renderWithRouter } from '../utils/renderWithRouter';
+
+test('Verifica se o menu inferior existe e contém os ícones', () => {
+  renderWithRouter(<Footer />);
+  const footer = screen.getByTestId('footer');
+  const drinksButton = screen.getByTestId('drinks-btn');
+  const mealsButton = screen.getByTestId('meals-btn');
+  const drinksImage = screen.getByTestId('drinks-bottom-btn');
+  const mealsImage = screen.getByTestId('meals-bottom-btn');
+
+  expect(footer).toBeInTheDocument();
+  expect(drinksButton).toBeInTheDocument();
+  expect(mealsButton).toBeInTheDocument();
+  expect(drinksImage).toHaveAttribute('src', '/src/images/drinkIcon.svg');
+  expect(mealsImage).toHaveAttribute('src', '/src/images/mealIcon.svg');
+});
+
+test('Verifica se o menu inferior está fixado ao final da página', () => {
+  renderWithRouter(<Footer />);
+  const footer = screen.getByTestId('footer');
+
+  expect(footer).toHaveStyle({
+    position: 'fixed',
+    bottom: '0',
+  });
+});
+
+test('Verifica se o clique no ícone de comidas redireciona corretamente', async () => {
+  renderWithRouter(<Footer />);
+
+  const mealsButton = screen.getByTestId('meals-bottom-btn');
+  await userEvent.click(mealsButton);
+
+  expect(window.location.pathname).toBe('/meals');
+});
+
+test('Verifica se o clique no ícone de bebidas redireciona corretamente', async () => {
+  renderWithRouter(<Footer />);
+
+  const drinksButton = screen.getByTestId('drinks-bottom-btn');
+  await userEvent.click(drinksButton);
+
+  expect(window.location.pathname).toBe('/drinks');
+});
