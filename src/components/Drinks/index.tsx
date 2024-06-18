2f4a407f76b527187b65bb504a7e0f8c1c46c68c
Eduardo Gonçalves Junior <eduardogjnr@gmail.com>
2024-06-18 17:00:31 -0300

    Drinks page finished.

--- src/components/Drinks/index.tsx
+++ src/components/Drinks/index.tsx
@@ -1,11 +1,17 @@
 import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { BsCupStraw } from 'react-icons/bs';
+import { MdOutlineLocalDrink } from 'react-icons/md';
+import { LiaCocktailSolid } from 'react-icons/lia';
+import { TbMilkshake } from 'react-icons/tb';
+import { LuCoffee } from 'react-icons/lu';
+import { CiCoffeeCup } from 'react-icons/ci';
 import RecipeCard from '../RecipeCard';
 import useFetchDrinks from '../../hooks/useFetchDrinks';
 import fetchDrinksByCategory from '../../utils/fetchDrinksByCategory';
 import { DrinkRecipeType } from '../../types';
 import DataContext from '../../context/DataContext';
+import './styles.css';
 
 function Drinks() {
   const { drinks, setDrinks, categories, setCategories } = useContext(DataContext);
@@ -36,33 +42,100 @@ function Drinks() {
     }
   };
 
+  const getCategoryImage = (categoryName: string) => {
+    const selected = 'text-primary-color';
+
+    switch (categoryName) {
+      case 'Ordinary Drink':
+        return (
+          <div>
+            <BsCupStraw
+              className={ selectedCategory === categoryName ? selected : '' }
+            />
+            <span className="category_text">{categoryName}</span>
+          </div>
+        );
+      case 'Cocktail':
+        return (
+          <div>
+            <LiaCocktailSolid
+              className={ selectedCategory === categoryName ? selected : '' }
+            />
+            <span className="category_text">{categoryName}</span>
+          </div>
+        );
+      case 'Shake':
+        return (
+          <div>
+            <TbMilkshake
+              className={ selectedCategory === categoryName ? selected : '' }
+            />
+            <span className="category_text">{categoryName}</span>
+          </div>
+        );
+      case 'Other / Unknown':
+        return (
+          <div>
+            <CiCoffeeCup
+              className={ selectedCategory === categoryName ? selected : '' }
+            />
+            <span className="category_text">{categoryName}</span>
+          </div>
+        );
+      case 'Cocoa':
+        return (
+          <div>
+            <LuCoffee
+              className={ selectedCategory === categoryName ? selected : '' }
+            />
+            <span className="category_text">{categoryName}</span>
+          </div>
+        );
+      default:
+        return (
+          <div>
+            <MdOutlineLocalDrink
+              className={ selectedCategory === categoryName ? selected : '' }
+            />
+            <span className="category_text">{categoryName}</span>
+          </div>
+        );
+    }
+  };
+
   return (
     <div className="container">
-      <div>
+      <div className="categories_grid">
         <h2>Categories</h2>
-        <button
-          data-testid="All-category-filter"
-          onClick={ () => handleCategoryChange('All') }
-        >
-          All
-        </button>
-        {categories && categories.map((category, index) => (
+        <div className="categories_wrapper">
           <button
-            key={ index }
-            data-testid={ `${category.strCategory}-category-filter` }
-            onClick={ () => handleCategoryChange(category.strCategory) }
+            data-testid="All-category-filter"
+            onClick={ () => handleCategoryChange('All') }
+            className="category_button button_all"
           >
-            {category.strCategory}
+            {getCategoryImage('All')}
           </button>
-        ))}
+          {categories && categories.map((category, index) => (
+            <button
+              key={ index }
+              data-testid={ `${category.strCategory}-category-filter` }
+              onClick={ () => handleCategoryChange(category.strCategory) }
+              className="category_button"
+            >
+              {getCategoryImage(category.strCategory)}
+            </button>
+          ))}
+        </div>
       </div>
       <div>
-        {
+        <div className="recipes_grid">
+          {
           drinks && selectedCategory === 'All'
             ? drinks.map((drink, index) => (
               <Link
                 key={ index }
                 to={ `${drink.idDrink}` }
+                className="recipe-link"
               >
                 <RecipeCard
                   data-testid={ `${index}-recipe-card` }
@@ -79,6 +152,7 @@ function Drinks() {
               <Link
                 key={ index }
                 to={ `${drink.idDrink}` }
+                className="recipe-link"
               >
                 <RecipeCard
                   data-testid={ `${index}-recipe-card` }
@@ -91,6 +165,7 @@ function Drinks() {
               </Link>
             ))
           }
+        </div>
       </div>
     </div>
   );
